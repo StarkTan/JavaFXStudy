433b5e0f2fb97ef402b5306b6e7868f1263504dc
Your Name <you@example.com>
2021-02-16 21:55:59 +0800

    lesson 001

---
+++ src/main/java/com/stark/study/lesson_001/Hello.java
@@ -0,0 +1,16 @@
+package com.stark.study.lesson_001;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Hello extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
