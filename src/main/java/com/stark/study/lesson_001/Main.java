06ff1603a762a5d102cbb7ef317c4e58e03fd917
Your Name <you@example.com>
2021-02-16 21:59:34 +0800

    lesson 001

--- src/main/java/com/stark/study/lesson_001/Main.java
+++ src/main/java/com/stark/study/lesson_001/Main.java
@@ -3,14 +3,15 @@ package com.stark.study.lesson_001;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Hello extends Application {
+public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
+        System.out.println("Hello World!");
+        primaryStage.setTitle("这是第一个JavaFX程序");
         primaryStage.show();
     }
 }
